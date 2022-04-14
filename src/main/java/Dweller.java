233d42c3c20bd71401338fb4545845dcb93994d5
Junmii <joy.krabbe@gmail.com>
2022-04-14 16:45:44 +0200

    First commit

---
+++ src/main/java/Dweller.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Dweller {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int gender;
+    private List<Integer> parents;
+}
