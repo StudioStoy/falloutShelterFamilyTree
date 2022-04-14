379caa5dae14279f2142b421ffaa215cbeaef28c
Junmii <joy.krabbe@gmail.com>
2022-04-14 17:30:19 +0200

    Updated JsonParser

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -1,2 +1,46 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class JsonParser {
+    public void parse() {
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+            FileReader reader = new FileReader("src/main/data/vault.json");
+
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+
+            JSONObject allInfo = (JSONObject) obj;
+
+            JSONObject dwellersObject = (JSONObject) allInfo.get("dwellers");
+            JSONArray dwellers = (JSONArray) dwellersObject.get("dwellers");
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    private static void parseDwellerObject(JSONObject dweller) {
+//        JSONObject dwellerObject = (JSONObject) dweller.get("dwellers");
+//
+//        //Get employee first name
+//        String id = (String) dwellerObject.get("serializeId");
+//        System.out.println(id);
+//
+//        //Get employee last name
+//        String firstName = (String) dwellerObject.get("name");
+//        System.out.println(firstName);
+//
+//        //Get employee website name
+//        String lastName = (String) dwellerObject.get("lastName");
+//        System.out.println(lastName);
+//    }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
 public class Main {
-
+    public static void main (String[] args) {
+        JsonParser jsonParser = new JsonParser();
+        jsonParser.parse();
+    }
 }
