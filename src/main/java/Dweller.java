c89f121f25fe42206c74764020ddee53f9d7c9df
stijuh <70705964+stijuh@users.noreply.github.com>
2022-04-14 17:31:46 +0200

    added dweller logic, .gitignore and lombok dependency

--- src/main/java/Dweller.java
+++ src/main/java/Dweller.java
@@ -1,9 +1,29 @@
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter @Setter
 public class Dweller {
     private int id;
     private String firstName;
     private String lastName;
-    private int gender;
+    private String gender;
     private List<Integer> parents;
+
+    public Dweller(int id, String firstName, String lastName, int gender, List<Integer> parents) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        if (gender == 1) {
+            this.gender = "female";
+        } else if (gender == 2) {
+            this.gender = "male";
+        }
+
+        if (parents.get(0) != -1 && parents.get(1) != -1) {
+            this.parents = parents;
+        }
+    }
 }
